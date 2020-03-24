7fdbb371716ebbcefac9bf0fe7b688ad573cac89
Marvin Beckmann <31038868+R3m1n0X@users.noreply.github.com>
2020-03-24 20:12:43 +0100

    Update docker_sql_restore.sh

--- docker_sql_restore.sh
+++ docker_sql_restore.sh
@@ -1,2 +1,2 @@
 # Oneliner to restore a SQL-Dump
-cat BACKUP_NAME.sql | docker exec -i CONTAINER_NAME /usr/bin/mysql -u root --password=root DATABASE_NAME
+cat BACKUP_NAME.sql | docker exec -i CONTAINER_NAME /usr/bin/mysql -u USER_NAME --password=P4ssW0rD DATABASE_NAME
